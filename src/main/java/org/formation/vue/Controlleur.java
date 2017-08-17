8f1e9e083bedb7f4e9d5092b9632370cbb2225c0
Brice <brice_adelin@hotmail.fr>
2017-08-17 11:14:59 +0200

    Brice_17aout_11h14

--- src/main/java/org/formation/vue/Controlleur.java
+++ src/main/java/org/formation/vue/Controlleur.java
@@ -1,10 +1,20 @@
 package org.formation.vue;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.formation.service.IClientComptesService;
 
@@ -19,6 +29,14 @@ public class Controlleur implements Serializable {
 
 	@Inject
 	private IClientComptesService clientComptesService;
+	
+	private static Logger LOGGER = LoggerFactory.getLogger(Controlleur.class);
 
+	public Controlleur() throws Exception {
+		super();
+	}
+
+	
+	
 	
 }
