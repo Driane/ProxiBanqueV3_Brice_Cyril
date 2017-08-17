3c8b05bed529c8a3790998db2c7f3a5cab56a6f8
Brice <brice_adelin@hotmail.fr>
2017-08-17 11:52:16 +0200

    Brice_17aout_11h52_controlleur

--- src/main/java/org/formation/service/IClientComptesService.java
+++ src/main/java/org/formation/service/IClientComptesService.java
@@ -8,26 +8,61 @@ import org.formation.model.CompteEpargne;
 
 public interface IClientComptesService {
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#listeInfosClients()
+	 */
 	List<Client> listeInfosClients() throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#infoClient(int)
+	 */
 	Client infoClient(int clientId) throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#ajouterClient(org.formation.model.Client)
+	 */
 	void ajouterClient(Client client) throws Exception;
 
+	void ajouterClientComptes(Client client, CompteCourant compteCourant, CompteEpargne compteEpargne) throws Exception;
+
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#miseAJourClient(org.formation.model.Client)
+	 */
 	void miseAJourClient(Client client) throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#supprimerClient(int)
+	 */
 	void supprimerClient(int clientId) throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#listeInfosCompteCourant()
+	 */
 	List<CompteCourant> listeInfosCompteCourant() throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#infoCompteCourant(int)
+	 */
 	CompteCourant infoCompteCourant(int compteCourantId) throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#miseAJourCompteCourant(org.formation.model.CompteCourant)
+	 */
 	void miseAJourCompteCourant(CompteCourant compteCourant) throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#listeInfosCompteEpargne()
+	 */
 	List<CompteEpargne> listeInfosCompteEpargne() throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#infoCompteEpargne(int)
+	 */
 	CompteEpargne infoCompteEpargne(int compteEpargneId) throws Exception;
 
+	/* (non-Javadoc)
+	 * @see org.formation.service.IClientComptesService#miseAJourCompteEpargne(org.formation.model.CompteEpargne)
+	 */
 	void miseAJourCompteEpargne(CompteEpargne compteEpargne) throws Exception;
 
 }
