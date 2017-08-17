de2a789141b967e55abc9cd524bd58e9a17dd08c
Driane <cyril.rabineau@gmail.com>
2017-08-17 11:21:00 +0200

    page xhtml terminé

---
+++ src/main/java/org/formation/vue/InputNumberView.java
@@ -0,0 +1,36 @@
+package org.formation.vue;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
+ 
+@ManagedBean
+@ViewScoped
+public class InputNumberView {
+ 
+
+    private Double montant = new Double(0);
+
+ 
+ 
+    public InputNumberView() {
+
+        montant = 0d;
+ 
+    }
+    
+    public void buttonAction(ActionEvent actionEvent) {
+        
+    }
+
+
+	public Double getMontant() {
+		return montant;
+	}
+
+
+
+	public void setMontant(Double montant) {
+		this.montant = montant;
+	}
+}
